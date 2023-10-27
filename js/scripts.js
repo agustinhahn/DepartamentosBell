371d03b7e1c692178f55f64d3e9f21ad70a72106
Agustin Hahn <agustin.f.hahn@gmail.com>
2023-10-26 22:09:38 -0300

    implementacion de contactos y mejora en servicios

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,29 @@ window.addEventListener('scroll', () => {
         navbar.classList.add('navbar-transparent');
     }
 });
+
+const accordionItems = document.querySelectorAll('.accordion-item');
+
+accordionItems.forEach((item) => {
+    const header = item.querySelector('.accordion-header');
+    const content = item.querySelector('.accordion-content');
+
+    header.addEventListener('click', () => {
+        if (content.classList.contains('active')) {
+            // Si el contenido actual está abierto, ciérralo
+            content.classList.remove('active');
+        } else {
+            // Si el contenido actual está cerrado, ciérralo y luego abre el actual
+            closeAllAccordionItems();
+            content.classList.add('active');
+        }
+    });
+});
+
+function closeAllAccordionItems() {
+    accordionItems.forEach((item) => {
+        const content = item.querySelector('.accordion-content');
+        content.classList.remove('active');
+    });
+}
+
